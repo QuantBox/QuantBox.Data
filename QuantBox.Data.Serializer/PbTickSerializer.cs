2f301877b4890e277ea887d6e0bd2f34a62ca540
hetao <yechaht@gmail.com>
2015-05-07 16:19:29 +0800

    reformat code

--- QuantBox.Data.Serializer/PbTickSerializer.cs
+++ QuantBox.Data.Serializer/PbTickSerializer.cs
@@ -13,84 +13,73 @@ namespace QuantBox.Data.Serializer
 {
     public class PbTickSerializer
     {
-        private V1.PbTickSerializer v1Reader = new V1.PbTickSerializer();
-        private V2.PbTickSerializer v2Reader = new V2.PbTickSerializer();
-        private V2.PbTickCodec v2Codec = new V2.PbTickCodec();
-        private int nVersion = 2;
-        private int nMaxVersion = 2;
-        
+        private readonly V1.PbTickSerializer _v1Reader = new V1.PbTickSerializer();
+        private readonly V2.PbTickSerializer _v2Reader = new V2.PbTickSerializer();
+        private readonly V2.PbTickCodec _v2Codec = new V2.PbTickCodec();
+        private int _version = 2;
+        private const int MaxVersion = 2;
+
         public V2.PbTick ReadOne(Stream stream)
         {
             // 计划如果简单的将V2和V1的文件合并成一个文件也能读取
-            long Position = stream.Position;
-            V2.PbTick tick = null;
-
-            bool bFisrtException = true;
-
-            do
-            {
-                try
-                {
-                    switch (nVersion)
-                    {
+            var position = stream.Position;
+            var bFisrtException = true;
+            do {
+                try {
+                    V2.PbTick tick;
+                    switch (_version) {
                         case 1:
-                            tick = V1_to_V2.Converter(v1Reader.ReadOne(stream));
+                            tick = V1_to_V2.Converter(_v1Reader.ReadOne(stream));
                             break;
                         case 2:
-                            tick = v2Reader.ReadOne(stream);
+                            tick = _v2Reader.ReadOne(stream);
                             break;
                         default:
-                            throw new Exception(string.Format("Invalid file, version {0}", nVersion));
+                            throw new Exception(string.Format("Invalid file, version {0}", _version));
                     }
                     return tick;
                 }
-                catch (ProtobufDataZeroException ex)
-                {
+                catch (ProtobufDataZeroException ex) {
                     bFisrtException = false;
-                    nVersion = ex.CurrentVersion;
-                    stream.Seek(Position, SeekOrigin.Begin);
+                    _version = ex.CurrentVersion;
+                    stream.Seek(position, SeekOrigin.Begin);
                 }
-                catch(ProtoException ex)
-                {
+                catch (ProtoException) {
                     // 第一次出现问题后，从最新格式开始向后进行解析
                     if (bFisrtException)
-                        nVersion = nMaxVersion;
+                        _version = MaxVersion;
                     else
-                        --nVersion;
+                        --_version;
 
                     bFisrtException = false;
-                    stream.Seek(Position, SeekOrigin.Begin);
+                    stream.Seek(position, SeekOrigin.Begin);
                 }
-            }while(true);
-
-            return tick;
+            } while (true);
         }
 
         public List<V2.PbTick> Read(Stream stream)
         {
-            List<V2.PbTick> _list = new List<V2.PbTick>();
+            var list = new List<V2.PbTick>();
 
-            while (true)
-            {
-                V2.PbTick tmp = ReadOne(stream);
-                if (tmp == null)
-                {
+            while (true) {
+                var tick = ReadOne(stream);
+                if (tick == null) {
                     break;
                 }
 
-                _list.Add(tmp);
+                list.Add(tick);
             }
-            return _list;
+            return list;
         }
 
         public V2.PbTickView ReadOne2View(Stream stream)
         {
-            return v2Codec.Data2View(ReadOne(stream), false);
+            return _v2Codec.Data2View(ReadOne(stream), false);
         }
 
         public List<V2.PbTickView> Read2View(Stream stream)
         {
-            return v2Codec.Data2View(Read(stream), false);
+            return _v2Codec.Data2View(Read(stream), false);
         }
     }
 }
